b769f3a4023b6f41f4cc19836170b4aff79ccd57
MostafaAbdelhalim <mostafamohamedabdelhalim@gmail.com>
2020-02-15 16:26:57 +0200

    Add files via upload
    Adding new constructors

---
+++ XOInterface.java
@@ -0,0 +1,83 @@
+package interfaces;
+
+import java.util.Vector;
+
+
+public class XOInterface {
+    
+    String typeOfOperation = null;
+    Boolean operationResult;
+    Player player = null;
+    public Gamelog gamelog = null;
+    public Vector<Player> Players= null;
+    int fieldNumber; 
+    //String homePlayer;
+    char signPlayed;
+    Boolean moveReceived;
+            
+    public XOInterface()
+    {
+        Players = new Vector<>();
+    }
+    
+    public XOInterface(String typeOfOperation,Player player)
+    {
+        this.typeOfOperation = typeOfOperation;        
+        this.player = player;
+    }
+    public XOInterface(String typeOfOperation,Gamelog gamelog)
+    {
+        this.typeOfOperation = typeOfOperation;        
+        this.gamelog = gamelog;
+    }
+    public XOInterface(String typeOfOperation,Gamelog gamelog,int fieldNumber,char signPlayed)
+    {
+        this.typeOfOperation = typeOfOperation;        
+        this.gamelog = gamelog;
+        this.fieldNumber=fieldNumber;
+        this.signPlayed=signPlayed;
+    }
+    public XOInterface(String typeOfOperation,Player player,Gamelog gamlog)
+    {
+        this.typeOfOperation = typeOfOperation;        
+        this.player = player;
+        this.gamelog = gamelog;
+    }
+    
+    public String getTypeOfOpearation()
+    {
+        return typeOfOperation;
+    }
+    public Boolean getOpearationResult()
+    {
+        return operationResult;
+        
+    }
+    
+    public Player getPlayer()
+    {
+        return player;
+    }
+    public Gamelog getGameLog()
+    {
+        return gamelog;
+    }    
+    
+    public void setFieldNumber (int fieldNumber)
+    {
+        this.fieldNumber = fieldNumber;
+    }
+    public void setSignPlayed (char signPlayed)
+    {
+        this.signPlayed = signPlayed;
+    }    
+    
+    public int getFieldNumber ()
+    {
+        return fieldNumber;
+    }
+    public char getSignPlayed ()
+    {
+        return signPlayed;
+    }        
+}
