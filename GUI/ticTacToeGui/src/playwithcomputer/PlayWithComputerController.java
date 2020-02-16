6fb3d06290902490139b6e6718710d237c77cce9
M-Hakim <mahmoud.abdelhakim9610@gmail.com>
2020-02-16 23:49:20 +0200

    merging single player mode with gui

--- GUI/ticTacToeGui/src/playwithcomputer/PlayWithComputerController.java
+++ GUI/ticTacToeGui/src/playwithcomputer/PlayWithComputerController.java
@@ -5,31 +5,189 @@
  */
 package playwithcomputer;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
  * @author E.S
  */
-public class PlayWithComputerController implements Initializable {
+public class PlayWithComputerController {
     
-    @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Button pos7;
+    @FXML
+    private Button pos8;
+    @FXML
+    private Button pos9;
+    @FXML
+    private Button pos6;
+    @FXML
+    private Button pos5;
+    @FXML
+    private Button pos4;
+    @FXML
+    private Button pos2;
+    @FXML
+    private Button pos3;
+    @FXML
+    private Button pos1;
+    @FXML
+    private ImageView pause;
+    @FXML
+    private static ImageView playerSign;
+    @FXML
+    private static ImageView AISign;
+
+    static char playerSymbol, AISymbol;
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    
+    Vector<Integer> playerMoves= new Vector<>();
+    Vector<Integer> AIMoves= new Vector<>();
+    static Vector<Integer> movesPool= new Vector<>();
+    static int numOfMoves;
+    static char getRndSymbol(){
+        Random r = new Random();
+        String symbols = "XO";
+        return symbols.charAt(r.nextInt(symbols.length()));
+    }
+    static boolean gameEnded;
+    boolean isWinningPosition(Vector<Integer> moves){
+        boolean winFlag = false;
+        Integer []  topRow = {1, 2, 3};
+        Integer []  midRow = {4, 5, 6};
+        Integer []  botRow = {7, 8, 9};
+        Integer []  leftCol = {1, 4, 7};
+        Integer []  midCol = {2, 5, 8};
+        Integer []  rightCol = {3, 6, 9};
+        Integer []  mainDiag = {1, 5, 9};
+        Integer []  secondaryDiag = {3, 5, 7};
+        Integer [][] winningCases = {
+                topRow, midRow, botRow,
+                leftCol, midCol, rightCol,
+                mainDiag, secondaryDiag
+        };
+
+        int i=0;
+        while(!winFlag && i<winningCases.length){
+            if(moves.containsAll(Arrays.asList(winningCases[i])))
+                winFlag = true;
+            i++;
+        }
+        return winFlag;
+    }
+    public static void init(){
+//        Image xImage = null, oImage = null;
+//        try {
+//            xImage = new Image(new FileInputStream("./src/playwithcomputer/Xsign.png"));
+//            oImage = new Image(new FileInputStream("./src/playwithcomputer/Osign.png"));
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("loading error");
+//        }
+        playerSymbol = getRndSymbol();
+        if (playerSymbol == 'X'){
+            AISymbol = 'O';
+//            playerSign.setImage(xImage);
+//            AISign.setImage(oImage);
+        }
+        else{
+            AISymbol = 'X';
+//            AISign.setImage(xImage);
+//            playerSign.setImage(oImage);
+        }
+        for(int i=0; i<9; i++)
+            movesPool.add(i+1);
+        numOfMoves = 0;
+        gameEnded = false;
+    }
+    Integer getRndMove() {
+        int number = (int) (Math.random() * movesPool.size());
+        return movesPool.get(number);
+    }
+    void displayMove(Integer position, char symbol){
+        switch (position) {
+            case 1:
+                pos1.setText(Character.toString(symbol));
+                break;
+            case 2:
+                pos2.setText(Character.toString(symbol));
+                break;
+            case 3:
+                pos3.setText(Character.toString(symbol));
+                break;
+            case 4:
+                pos4.setText(Character.toString(symbol));
+                break;
+            case 5:
+                pos5.setText(Character.toString(symbol));
+                break;
+            case 6:
+                pos6.setText(Character.toString(symbol));
+                break;
+            case 7:
+                pos7.setText(Character.toString(symbol));
+                break;
+            case 8:
+                pos8.setText(Character.toString(symbol));
+                break;
+            case 9:
+                pos9.setText(Character.toString(symbol));
+                break;
+            default:
+                break;
+        }
+    }
+    @FXML
+    void playMove(ActionEvent event) {
+        if (!gameEnded) {
+            // Player move
+            Integer playerPos = Integer.parseInt(((Control) event.getSource()).getId());
+            if (!movesPool.isEmpty() && movesPool.contains(playerPos)) {
+                displayMove(playerPos, playerSymbol);
+                movesPool.remove(playerPos);
+                playerMoves.add(playerPos);
+                numOfMoves++;
+                if(isWinningPosition(playerMoves)){
+                    System.out.println("You win! :D");
+                    gameEnded = true;
+                }
+            }
+            // AI move
+            if (!movesPool.isEmpty() && !gameEnded) {
+                Integer AIPos = getRndMove();
+                displayMove(AIPos, AISymbol);
+                movesPool.remove(AIPos);
+                AIMoves.add(AIPos);
+                numOfMoves++;
+                if(isWinningPosition(AIMoves)){
+                    System.out.println("You Lose! :(");
+                    gameEnded = true;
+                }
+            }
+            if (numOfMoves >= 9){
+                System.out.println("It's a draw!");
+                gameEnded = true;
+            }
+        }
+    }
+
+    @FXML
+    private void pause(MouseEvent event) {
+    }
     
 }
--- GUI/ticTacToeGui/src/tictactoegui/TicTacToeGui.java
+++ GUI/ticTacToeGui/src/tictactoegui/TicTacToeGui.java
@@ -1,13 +1,19 @@
 
 package tictactoegui;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import playwithcomputer.PlayWithComputerController;
 import signin.*;
 import signup.*;
 
@@ -18,9 +24,13 @@ public class TicTacToeGui extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent  root = FXMLLoader.load(getClass().getResource("/signin/signIn.fxml"));
+//        Parent  root = FXMLLoader.load(getClass().getResource("/signin/signIn.fxml"));
+
+        Parent  root = FXMLLoader.load(getClass().getResource("/playwithcomputer/playWithComputer.fxml"));
         
         Scene scene = new Scene(root);
+        scene.getStylesheets().add("/playwithcomputer/style.css");
+        PlayWithComputerController.init();
         
         stage.setScene(scene);
         stage.show();
@@ -28,7 +38,8 @@ public class TicTacToeGui extends Application {
     
   
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
         Application.launch(args);
     }
     
