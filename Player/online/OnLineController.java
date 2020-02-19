b1a4a62c5a4049c9c9f79c5635298ce56eea9d92
Mayar9 <mayar7assan567@gmail.com>
2020-02-19 19:12:35 +0200

    added the invitation logic

--- Player/online/OnLineController.java
+++ Player/online/OnLineController.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package online;
 
+import com.google.gson.Gson;
+import interfaces.Gamelog;
 import java.io.DataInputStream;
 import java.io.PrintStream;
 import java.net.URL;
@@ -13,17 +10,34 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import signin.SignInController;
+import interfaces.*;
 
-/**
- *
- * @author E.S
- */
 public class OnLineController implements Initializable {
-       DataInputStream controllerDIS;
+    
+    
+    DataInputStream controllerDIS;
     PrintStream controllerPS;
+    String homePlayer = SignInController.username;
+    String opponentPlayer;
+    
+    
     public void setControllerStreams(DataInputStream dis, PrintStream ps){
         controllerDIS = dis;
         controllerPS = ps;
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        
+        //opponentPlayer 
+        Gamelog offlineGameCreation=new Gamelog();
+        offlineGameCreation.setHomePlayer(homePlayer);
+        offlineGameCreation.setOpponentPlayer(opponentPlayer);
+        XOInterface xointerface =new XOInterface ("invite",offlineGameCreation);
+        Gson g = new Gson();
+        String s = g.toJson(xointerface);
+        controllerPS.println(s);
     }
     
     @Override
