fabba0ca0cf6856f433ea5b67150c59de15bc41f
Amr Walid <amrwsk@gmail.com>
2020-02-20 16:20:31 +0200

    Chaned The LayOut Of The Offline Page

--- Player/playwithcomputer/PlayWithComputerController.java
+++ Player/playwithcomputer/PlayWithComputerController.java
@@ -28,6 +28,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import signin.SignInController;
 
 /**
@@ -55,12 +56,6 @@ public class PlayWithComputerController implements Initializable {
     private Button pos3;
     @FXML
     private Button pos1;
-    @FXML
-    private ImageView pause;
-    @FXML
-    private static ImageView playerSign;
-    @FXML
-    private static ImageView AISign;
 
     DataInputStream controllerDIS;
     PrintStream controllerPS;
@@ -70,6 +65,14 @@ public class PlayWithComputerController implements Initializable {
     Vector<Integer> AIMoves= new Vector<>();
     Vector<Integer> movesPool= new Vector<>();
     int numOfMoves;
+    @FXML
+    private Label userNameLabel;
+    @FXML
+    private Label gameResult;
+    @FXML
+    private Text playerSign;
+    @FXML
+    private Text computerSign;
     char getRndSymbol(){
         Random r = new Random();
         String symbols = "XO";
@@ -111,15 +114,19 @@ public class PlayWithComputerController implements Initializable {
         else{
             AISymbol = 'X';
         }
+        playerSign.setText(Character.toString(playerSymbol));
+        computerSign.setText(Character.toString(AISymbol));
         for(int i=0; i<9; i++)
             movesPool.add(i+1);
         numOfMoves = 0;
         gameEnded = false;
     }
+    
     Integer getRndMove() {
         int number = (int) (Math.random() * movesPool.size());
         return movesPool.get(number);
     }
+    
     void displayMove(Integer position, char symbol){
         switch (position) {
             case 1:
@@ -165,6 +172,7 @@ public class PlayWithComputerController implements Initializable {
                 numOfMoves++;
                 if(isWinningPosition(playerMoves)){
                     System.out.println("You win! :D");
+                    gameResult.setText("You Win! :D");
                     gameEnded = true;
                     reportGameEnding();
                 }
@@ -178,19 +186,18 @@ public class PlayWithComputerController implements Initializable {
                 numOfMoves++;
                 if(isWinningPosition(AIMoves)){
                     System.out.println("You Lose! :(");
+                    gameResult.setText("You Lose! :(");
                     gameEnded = true;
                 }
             }
             if (numOfMoves >= 9){
                 System.out.println("It's a draw!");
+                gameResult.setText("It's a Draw! ");
                 gameEnded = true;
             }
         }
     }
 
-    @FXML
-    private void pause(MouseEvent event) {
-    }
     public void setControllerStreams(DataInputStream dis, PrintStream ps){
         controllerDIS = dis;
         controllerPS = ps;
@@ -204,5 +211,29 @@ public class PlayWithComputerController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         init();
+        userNameLabel.setText(myUserName);
+    }
+
+    @FXML
+    private void back(ActionEvent event) {
+    }
+
+    @FXML
+    private void restart(ActionEvent event) {
+        init();
+        clearAll();
+    }
+    void clearAll ()
+    {
+        pos1.setText("");
+        pos2.setText("");
+        pos3.setText("");
+        pos4.setText("");
+        pos5.setText("");
+        pos6.setText("");
+        pos7.setText("");
+        pos8.setText("");
+        pos9.setText("");
+        gameResult.setText("");
     }
 }
