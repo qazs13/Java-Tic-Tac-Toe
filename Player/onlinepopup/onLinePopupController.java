f0016fd044923a3c6efe87e4aa635af9b888055a
ayamohamed1993 <aya_abouelkasem@yahoo.com>
2020-02-20 18:58:50 +0200

    editing on onlinepopup page

--- Player/onlinepopup/onLinePopupController.java
+++ Player/onlinepopup/onLinePopupController.java
@@ -3,19 +3,28 @@ import java.io.DataInputStream;
 import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
 public class onLinePopupController implements Initializable {
 
     @FXML
     private Text poptext;
+    
+          DataInputStream controllerDIS;
+    PrintStream controllerPS;
     String uname;
-
+  
+    public void setControllerStreams(DataInputStream dis, PrintStream ps){
+        controllerDIS = dis;
+        controllerPS = ps;
+    }
      public void getusername( String username){
-       uname = username;
-       poptext.setText(uname + " is now on line");
+       uname=username;
+       poptext.setText(uname + "is now on line");
      }
     
    
